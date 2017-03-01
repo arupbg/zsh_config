a401967c7953628ebe04251e39ee8af8118da38d
EnriqueAlbertoBetancourtGarcia <fis.ebetancourt@gmail.com>
2017-03-01 02:10:11 -0600

    More general version of vimfix, I think it will work with most linux users

--- vimfix.py
+++ vimfix.py
@@ -1,16 +1,27 @@
 import os
+import getpass
+import sys
 
-path = '/home/enrique/.vim'
-lstvim = []
+name = getpass.getuser()
 
+path = '/home/%s'%name + '/.vim'
 
-for dirName, subdirList, fileList in os.walk(path):
-    for fileName in fileList:
-        if ".vim" in fileName.lower() in fileList:
-            file = os.path.join(dirName,fileName)
-            print file 
-            os.system("zsh -c 'dos2unix -f %s'"%file)
-            #lstvim.append(os.path.join(dirName,fileName))
+def Looper(term):
+    for dirName, subdirList, fileList in os.walk(path):
+        for fileName in fileList:
+            if ".vim" in fileName.lower() in fileList:
+                file = os.path.join(dirName,fileName)
+                os.system("%s "%term + "-c 'dos2unix -f %s'"%file) 
 
-
-
+if os.path.isfile('/usr/bin/zsh'):
+    Looper("zsh")
+    sys.exit('Probably the job is done!')
+elif os.path.isfile('/usr/bin/bash'):
+    Looper("bash")
+    sys.exit('Probably the job is done!')
+else:
+    print "We haven't found your terminal interpreter a.k.a (bash,zsh,tmux etc.. etc..)"
+    tname = raw_input("Please write the path of your interpreter ex: /usr/bin/bash \n")
+    Looper(tname)
+    sys.exit('Probably the job is done!')
+    
