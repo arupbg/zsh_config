8515114480d7497f259a1ad8278cccc19622d46e
Enrique Betancourt <fis.ebetancourt@gmail.com>
2017-03-01 01:35:07 -0600

    Updated :D

--- vimfix.py
+++ vimfix.py
@@ -1,3 +1,7 @@
+# Plugin Vim Fixer
+# Fixes ^M and other errors provided by Windows text editors
+# It may also cure stress!
+
 import os
 
 path = '/home/enrique/.vim'
@@ -8,7 +12,7 @@ for dirName, subdirList, fileList in os.walk(path):
     for fileName in fileList:
         if ".vim" in fileName.lower() in fileList:
             file = os.path.join(dirName,fileName)
-            print file 
+            #print file 
             os.system("zsh -c 'dos2unix -f %s'"%file)
             #lstvim.append(os.path.join(dirName,fileName))
 
